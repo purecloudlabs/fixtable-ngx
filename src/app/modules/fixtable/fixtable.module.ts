7f2996b3d64f5d5e20f9e76f290584d93cf8c1e5
Gabriel Elkind <gwelkind@gmail.com>
2018-04-12 11:15:24 -0500

    Is able to be imported by admin-ui-v3

--- src/app/modules/fixtable/fixtable.module.ts
+++ src/app/modules/fixtable/fixtable.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GridComponent } from './grid/grid.component';
-import {ColumnHeaderComponent} from "./column-header/column-header.component";
+import { ColumnHeaderComponent } from "./column-header/column-header.component";
 
 @NgModule({
   imports: [
---
+++ src/public_api.ts
@@ -0,0 +1,1 @@
+export * from './app/modules/fixtable/fixtable.module';
