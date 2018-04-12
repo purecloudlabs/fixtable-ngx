b75ac941ac2238da5808cbc35d2b3d5c17b072cb
Gabriel Elkind <gwelkind@gmail.com>
2018-04-12 16:15:02 -0500

    Adds a postinstall, ignores dist

--- dist/fixtable-fangular.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public_api';
-export { ColumnHeaderComponent as ɵb } from './app/modules/fixtable/column-header/column-header.component';
-export { GridComponent as ɵa } from './app/modules/fixtable/grid/grid.component';
